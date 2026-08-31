7ff8542f9d50dd984454e9642c5097d4646e3f76
Nicolinho <nicolaslinaresoli@gmail.com>
2026-08-31 19:08:41 -0300

    ajuste identação

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 import Slide from "./slide.js";
 
-const slide = new Slide('.slide', '.wrapper');
+const slide = new Slide(".slide", ".wrapper");
 slide.init();
 
-slide.changeSlide(3)
+slide.changeSlide(3);
