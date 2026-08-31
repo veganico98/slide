37d1f688273d70f7604e5249c4163ba362566795
Nicolinho <nicolaslinaresoli@gmail.com>
2026-08-31 11:47:30 -0300

    inicio eventos

---
+++ js/slide.js
@@ -0,0 +1,39 @@
+export default class Slide {
+    constructor(slide, wrapper) {
+        this.slide = document.querySelector(slide);
+        this.wrapper = document.querySelector(wrapper);
+        console.log(this.slide, this.wrapper)
+    }
+
+    onStart(event) {
+        event.preventDefault();
+        console.log(event);
+        this.wrapper.addEventListener('mousemove', this.onMove);
+    }
+
+    onMove(event) {
+        console.log('moveu');
+    }
+
+    onEnd(event) {
+        console.log('acabou');
+        this.wrapper.removeEventListener('mousemove', this.onMove)
+    }
+
+    addSlideEvents() {
+        this.wrapper.addEventListener('mousedown', this.onStart);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
+    }
+
+    bindEvents() {
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+    }
+
+    init() {
+        this.bindEvents();
+        this.addSlideEvents();
+        return this;
+    }
+}
